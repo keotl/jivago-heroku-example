aec9078d9a22d863932cdd1cd059d60a22585691
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-09-02 12:06:39 -0400

    debug=True

--- hello_world/wsgi.py
+++ hello_world/wsgi.py
@@ -2,7 +2,7 @@ from jivago.jivago_application import JivagoApplication
 
 import hello_world.app
 
-application = JivagoApplication(hello_world.app)
+application = JivagoApplication(hello_world.app, debug=True)
 
 if __name__ == '__main__':
     application.run_dev()
