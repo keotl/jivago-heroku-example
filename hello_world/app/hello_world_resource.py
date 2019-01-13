d9c7b3a3e214cf1ae7866bbde3fd1bc882dcf0db
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-01-13 10:54:57 -0500

    jivago 0.5.0

--- hello_world/app/hello_world_resource.py
+++ hello_world/app/hello_world_resource.py
@@ -1,5 +1,6 @@
 from jivago.templating.rendered_view import RenderedView
 from jivago.wsgi.annotations import Resource, Path
+from jivago.wsgi.invocation.parameters import PathParam
 from jivago.wsgi.methods import GET
 
 
@@ -12,10 +13,10 @@ class HelloWorldResource(object):
 
     @GET
     @Path("/{name}")
-    def path_named_hello(self, name: str) -> str:
+    def path_named_hello(self, name: PathParam[str]) -> str:
         return "Hello {}!".format(name)
 
     @GET
     @Path("/{name}/template")
-    def named_hello_with_a_template(self, name: str) -> RenderedView:
+    def named_hello_with_a_template(self, name: PathParam[str]) -> RenderedView:
         return RenderedView("named_hello.html", {"name": name})
